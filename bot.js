3c9f7a485b249b78c14a2df2e8dcd3ff4316b432
yurii <uratyvodar@Uras-MacBook-Pro.local>
2026-06-07 15:47:58 +0300

    fix: more spacing around divider

--- bot.js
+++ bot.js
@@ -2051,7 +2051,7 @@ async function updateUserPin(userId) {
     }
 
     if (takenJobs.length) {
-      pinText += `\n▱▱▱\n`;
+      pinText += `\n▱▱▱\n\n`;
       pinText += `👀 *Being done for you*\n`;
       takenJobs.forEach(j => {
         const workerName = j.workerName || '';
